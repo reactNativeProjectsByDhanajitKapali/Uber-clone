6a46b9acaaa2fab4f4dcdc05f43b66a366bcc4ed
dhanajitkapali <dhanajitkapali30@gmail.com>
2022-04-04 18:55:39 +0530

    added google cloud api

--- Screens/MapScreen.js
+++ Screens/MapScreen.js
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import React from "react";
 
 const MapScreen = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>MapScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
